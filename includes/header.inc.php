a612b0a552560019171963f39ed5f99a75ff6e71
Guillaume Bellemare <gbellemare@propagandadesign.com>
2014-10-06 10:42:29 -0400

    Ajout d'icon apple par défaut

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,5 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <meta name="apple-mobile-web-app-title" content="">
-<link rel="apple-touch-icon" href=""/>
+<link rel="apple-touch-icon" href="<?php echo URL_ROOT; ?>images/common/apple-touch-icon.png"/>
 <link rel="shortcut icon" href="<?php echo URL_ROOT; ?>images/common/favicon.ico" type="image/x-icon">
 <?php echo $compressor->load('css', array('styles/reset.css', 'styles/layout.css', 'styles/global.css', 'styles/flexslider.css', 'styles/jquery.fancybox.css', 'styles/queries.css'), 'styles/min/global-compress.min.css', false);
