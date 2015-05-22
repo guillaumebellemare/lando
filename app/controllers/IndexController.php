c07c8936e0f79757ea0e60077d8a7cdd08499272
Guillaume Bellemare <gbellemare@propagandadesign.com>
2015-05-22 16:32:04 -0400

    Multiple

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,7 +4,7 @@ class IndexController extends AppController {
 
 	function index() {
 		
-		global $routes, $lang2, $lang3;
+		global $lang2, $lang3;
 		
 		# Model declaration
 		$base = new Base();
@@ -13,11 +13,7 @@ class IndexController extends AppController {
 		
 		# Data
 		//$foo = $base->getAllBar();
-		/*$this->setTitle("Title");
-		$this->setDescription("Description");
-		$this->setKeywords("key, words");
-		$this->setPageType("article");
-		$this->setImage("image.jpg");*/
+
 		//return array("foo" => $foo);
 	}
 	
