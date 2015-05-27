ff3a3c12a1ea2ab126cb0931bbcb3b65e657264f
Guillaume Bellemare <gbellemare@propagandadesign.com>
2015-05-27 13:42:26 -0400

    Login controller now active

--- public/includes/messages.php
+++ public/includes/messages.php
@@ -1,13 +1,27 @@
 <?php
 
-if($_SESSION['errors']) { $errors[] = $_SESSION['errors']; unset($_SESSION['errors']); }
-if($_SESSION['messages']) { $messages[] = $_SESSION['messages']; unset($_SESSION['messages']); }
+if($_SESSION['errors'])
+{
+	foreach($_SESSION['errors'] as $session_error)
+	{
+		$errors[] = $session_error;
+	}
+}
+
+if($_SESSION['messages'])
+{
+	foreach($_SESSION['messages'] as $session_messages)
+	{
+		$messages[] = $session_messages;
+	}
+}
+
 
 if($errors)
 {
 	echo '<div class="msg is-failure">';
 		foreach ($errors as $error) {
-			echo '<p><span class="msg-icon"></span>'.$error.'</p>';
+			echo '<p>'.$error.'</p>';
 		}
 	echo '</div>';
 }
@@ -16,7 +30,10 @@ if($messages)
 {
 	echo '<div class="msg is-success">';
 		foreach ($messages as $message) {
-			echo '<span class="msg-icon"></span>'.$message;
+			echo '<p>'.$message.'</p>';
 		}
 	echo '</div>';
 }
+
+if($_SESSION['errors']) { unset($_SESSION['errors']); }
+if($_SESSION['messages']) { unset($_SESSION['messages']); }
---
+++ public/lang/fr/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$login = array(
+	"recover.not_same" => "Les deux mots de passes doivent être identiques.",
+	"recover.not_secure" => "Votre nouveau mot de passe doit être composé au moins de 8 caractère, une lettre majuscule, une lettre minuscule et un nombre.",
+	"recover.success" => "Votre mot de passe a bien été changé.",
+	"login" => "Se connecter",
+	"logout" => "Se déconnecter",
+);
