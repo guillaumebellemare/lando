c686b9b99d070e3b658435dca423760eeab6934d
Guillaume Bellemare <gbellemare@propagandadesign.com>
2015-08-07 15:19:54 -0400

    Translation update

--- app/helpers/translate.class.php
+++ app/helpers/translate.class.php
@@ -6,7 +6,10 @@ class Translate {
 	{
 		global $routes, $lang2_trans, $lang2;
 		
-		if(isset($_GET['param1'])) $key = array_search("{$_GET['page']}/{$_GET['param1']}", $routes);
+		if(isset($_GET['param1']) && isset($_GET['param2']) && isset($_GET['param3']) && isset($_GET['param4'])) $key = array_search("{$_GET['page']}/{$_GET['param1']}/{$_GET['param2']}/{$_GET['param3']}/{$_GET['param4']}", $routes);
+		elseif(isset($_GET['param1']) && isset($_GET['param2']) && isset($_GET['param3'])) $key = array_search("{$_GET['page']}/{$_GET['param1']}/{$_GET['param2']}/{$_GET['param3']}", $routes);
+		elseif(isset($_GET['param1']) && isset($_GET['param2'])) $key = array_search("{$_GET['page']}/{$_GET['param1']}/{$_GET['param2']}", $routes);
+		elseif(isset($_GET['param1'])) $key = array_search("{$_GET['page']}/{$_GET['param1']}", $routes);
 		elseif(!$key) $key = array_search("{$_GET['page']}", $routes);
 		
 		if($routes["$key"] && $routes["$key"] != 'index')
