22130de41ee6328fa304b6d95b0d02e894dbe920
Guillaume Bellemare <gbellemare@propagandadesign.com>
2015-11-30 09:35:52 -0500

    responsive-request-desktop-site

--- public/includes/scripts.php
+++ public/includes/scripts.php
@@ -3,6 +3,7 @@
 <script src="<?=URL_ROOT . PUBLIC_FOLDER?>scripts/selectivizr-min.js" type="text/javascript"></script>
 <script src="<?=URL_ROOT . PUBLIC_FOLDER?>scripts/navigation.js" type="text/javascript"></script>
 <script src="<?=URL_ROOT . PUBLIC_FOLDER?>scripts/jquery.smooth-scroll.js" type="text/javascript"></script>
+<script src="<?=URL_ROOT . PUBLIC_FOLDER?>scripts/responsive-request-desktop-site.js" type="text/javascript"></script>
 <script>
 /* Animated navigation-trigger */
 $(document).ready(function(){
